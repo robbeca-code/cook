a03f1b26ca3f6a8299dcfd1eec27ac8e2c314f66
robbeca <xmfkddl1006@gmail.com>
2022-12-06 22:42:15 +0900

    Created the structure of the homepage.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
+import Slide from './data/slide';
+import Tag from './data/tag';
+import Content from './data/content';
+import Nav from './data/navigation';
 
 function App() {
   return (
@@ -28,62 +32,12 @@ function App() {
         <Slide />
 
         <Tag />
+
+        <Content />
+
+        <Nav />
       </header>
 
-    </div>
-  );
-}
-
-function Slide() {
-  let [slide, setSlide] = useState([
-    {
-      img: '/public-assets/slide1.png',
-      title: '자취생! 무얼 먹을지 고민이라면',
-      content: 'XX찌개 키트 세트는 어떠세요?'
-    },
-    {
-      img: '/public-assets/slide1.png',
-      title: '집들이 음식이 고민이라면',
-      content: 'XX찌개 키트 세트는 어떠세요?'
-    }
-  ]);
-
-  return (
-    <section className="slide-container">
-      {
-        slide.map((slide, i) => {
-          return (
-            <div className="slide" key={'slide' + i}>
-              <img 
-                src={slide.img}
-                alt="AD image1" 
-                className="slide-img"/>
-              <div className="slide-font-container">
-                <h2 className="slide-font title">{slide.title}</h2>
-                <span className="slide-font">{slide.content}</span>
-              </div>
-            </div>
-          );
-        })
-      }
-    </section>
-  );
-}
-
-function Tag() {
-  let tag = ['#닭요리', '#저녁_반찬_추천', '#찌개', '#찜닭_재료', '#삼계탕', '#천연조미료', '#알리오올리오'];
-
-  return(
-    <div className="tag-container">
-      {
-        tag.map((item, i) => {
-          return(
-            <button type="button" className="btn tag-btn" key={'tag' + i}>
-              {item}
-            </button>
-          );
-        })
-      }
     </div>
   );
 }
