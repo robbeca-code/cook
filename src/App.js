1baa9ca5831a8a1f9af2b9f5347472c63a357fb8
robbeca <xmfkddl1006@gmail.com>
2022-12-07 22:15:53 +0900

    Created a slide image.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import React from 'react';
 import { useState } from 'react';
-import Slide from './data/slide';
-import Tag from './data/tag';
-import Content from './data/content';
-import Nav from './data/navigation';
+import {slide, tag, content} from './data/data';
 
 function App() {
   return (
@@ -15,7 +12,7 @@ function App() {
             <img
               src={process.env.PUBLIC_URL + '/public-assets/menu.png'}
               alt="menu icon"
-              className="icon menu" />
+              className="top-icon menu" />
           </a>
           <img 
             src={process.env.PUBLIC_URL + '/public-assets/logo.png'}
@@ -25,15 +22,15 @@ function App() {
             <img 
               src={process.env.PUBLIC_URL + '/public-assets/search.png'}
               alt="search icon"
-              className="icon search" />
+              className="top-icon search" />
           </a>
         </nav>
 
-        <Slide />
+        <Slide slide={slide} />
 
-        <Tag />
+        <Tag tag={tag} />
 
-        <Content />
+        <Content content={content} />
 
         <Nav />
       </header>
@@ -42,4 +39,140 @@ function App() {
   );
 }
 
+
+function Slide(props) {
+  let [index, setIndex] = useState(0);
+
+  function onChangeImage(counter) {
+    console.log(index +', '+counter);
+    if(counter >= props.slide.length){
+      index = 0;
+      setIndex(index);
+      return;
+    } else if(counter < 0) {
+      index = props.slide.length - 1;
+      setIndex(index);
+      return;
+    }
+
+    setIndex(counter);
+  }
+
+  return (
+    <section className="slide-container">
+      <div className="slide" style={{
+        left: '0%',
+        transform: `translateX(-${index * 100}%)`
+      }}>
+        <img 
+          src={props.slide[0].img}
+          alt="AD image1" 
+          className="slide-img"/>
+        <div className="slide-font-container">
+          <h2 className="slide-font title">{props.slide[0].title}</h2>
+          <span className="slide-font">{props.slide[0].content}</span>
+        </div>
+      </div>
+      <div className="slide" style={{
+        left: '100%',
+        transform: `translateX(-${index * 100}%)`
+      }}>
+        <img 
+          src={props.slide[1].img}
+          alt="AD image1" 
+          className="slide-img"/>
+        <div className="slide-font-container">
+          <h2 className="slide-font title">{props.slide[1].title}</h2>
+          <span className="slide-font">{props.slide[1].content}</span>
+        </div>
+      </div>
+      <button type="button" className="btn prev-btn" onClick={() => {
+        onChangeImage(index - 1);
+      }}>
+        <img src="/public-assets/prev.png" alt="Preview button" />
+      </button>
+      <button type="button" className="btn next-btn" onClick={() => {
+        onChangeImage(index + 1);
+      }}>
+        <img src="/public-assets/next.png" alt="Next button" />
+      </button>
+    </section>
+  );
+}
+
+function Tag(props) {
+  return(
+    <div className="tag-container">
+      {
+        props.tag.map((item, i) => {
+          return(
+            <button type="button" className="btn tag-btn" key={'tag' + i}>
+              {item}
+            </button>
+          );
+        })
+      }
+    </div>
+  );
+}
+
+function Content(props) {
+
+  return (
+    <section className="app-contents">
+      {
+        props.content.map((item, i) => {
+          return (
+            <article className="content" key={i}>
+              <h2 className="sub-title">{item.subTitle}</h2>
+              <div className="content-header">
+                <h1 className="main-title">{item.mainTitle} <span className="point">{item.styleTitle}</span></h1>
+                <img src="/public-assets/bookmark.png" alt="bookrmark icon" className="bookmark" />
+              </div>
+              <div className="content-main">
+                <div className="img-container">
+                  <img src={item.img1} alt="" />
+                </div>
+                {
+                  item.img2 === undefined ? null : 
+                    <div className="img-container">
+                      <img src={item.img2} alt="" />
+                    </div>
+                }
+                
+              </div>
+            </article>
+          );
+        })
+      }
+    </section>
+  );
+}
+
+function Nav() {
+
+  return(
+    <ol className="gnb">
+      <li className="gnb-item">
+        <img src="/public-assets/nav/click-home.png" alt="home icon" className="gnb-icon"/>
+        <span className="gnb-title click">홈</span>
+      </li>
+      <li className="gnb-item">
+        <img src="/public-assets/nav/recipe.png" alt="recipe icon" className="gnb-icon"/>
+        <span className="gnb-title">레시피</span>
+      </li>
+      <li className="gnb-item">
+        <img src="/public-assets/nav/main.png" alt="main icon" className="gnb-main"/>
+      </li>
+      <li className="gnb-item">
+        <img src="/public-assets/nav/chat.png" alt="chatting icon" className="gnb-icon"/>
+        <span className="gnb-title">채팅</span>
+      </li>
+      <li className="gnb-item">
+        <img src="/public-assets/nav/mypage.png" alt="mypage icon" className="gnb-icon"/>
+        <span className="gnb-title">마이페이지</span>
+      </li>
+    </ol>
+  );
+}
 export default App;
---
+++ src/data/data.js
@@ -0,0 +1,40 @@
+let slide = [
+  {
+    img: '/public-assets/slide1.png',
+    title: '자취생! 무얼 먹을지 고민이라면',
+    content: 'fresheasy의 찌개 세트는 어떠세요?'
+  },
+  {
+    img: '/public-assets/slide2.png',
+    title: '집들이 음식이 고민이라면',
+    content: '셰프의 요리키트를 사용해보세요!'
+  }
+];
+
+let tag = ['#닭요리', '#저녁_반찬_추천', '#찌개', '#찜닭_재료', '#삼계탕', '#천연조미료', '#알리오올리오'];
+
+let content = [
+  {
+    subTitle: 'Today!',
+    mainTitle: '오늘의',
+    styleTitle: '셰프 요리',
+    img1: '/public-assets/chef-cook1.jpg',
+    img2: '/public-assets/chef-cook2.jpg'
+  },
+  {
+    subTitle: '음식과 함께',
+    mainTitle: '건강한',
+    styleTitle: '다이어트',
+    img1: '/public-assets/healthy1.jpg',
+    img2: '/public-assets/healthy2.jpg'
+  },
+  {
+    subTitle: '영상으로 확인하는',
+    mainTitle: '특별한',
+    styleTitle: '저녁시간',
+    img1: '/public-assets/healthy1.jpg'
+  },
+];
+
+
+export {slide, tag, content};
